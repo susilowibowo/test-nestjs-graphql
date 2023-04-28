a7d20b978d7e0335fa801076472f2591ec169ae9
susilowb <susilohdwibowo@gmail.com>
2023-04-28 08:49:59 +0700

    test-7 adding db transaction or queryrunner

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { Repository, DataSource } from 'typeorm';
 import { Partner } from 'src/partners/entities/partner.entity';
 
 @Injectable()
@@ -11,35 +11,52 @@ export class UsersService {
     private userRepository: Repository<User>,
     @InjectRepository(Partner)
     private partnerRepository: Repository<Partner>,
+    private dataSource: DataSource,
   ) {}
 
   async registerUser(userInput: any): Promise<boolean> {
     const { name, email, phone, marketing_code, partner_type_id } = userInput;
-    
-    // create user first
-    const user = new User();
-    user.name = name;
-    user.email = email;
-    user.phone = phone;
-    user.username = email;
-    user.role_id = 11;
-    await this.userRepository.save(user);
 
-    // find marketing by marketing_code (no entity so use partnerRepository)
-    const marketing = await this.partnerRepository.query(
-      'SELECT * FROM marketing WHERE code = ? LIMIT 1',
-      [marketing_code],
-    );
+    // create query runner to run transaction
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
-    if (!marketing || marketing === undefined || marketing.length === 0) {
-      return false; // return false if marketing entity is not found
+    try {
+      // create user first
+      const user = new User();
+      user.name = name;
+      user.email = email;
+      user.phone = phone;
+      user.username = email;
+      user.role_id = 11;
+      await queryRunner.manager.save(user);
+
+      // find marketing by marketing_code (no entity so use partnerRepository)
+      const marketing = await this.partnerRepository.query(
+        'SELECT * FROM marketing WHERE code = ? LIMIT 1',
+        [marketing_code],
+      );
+
+      if (!marketing || marketing === undefined || marketing.length === 0) {
+        return false; // return false if marketing entity is not found
+      }
+
+      const partner = new Partner();
+      partner.user = user;
+      partner.marketing_id = marketing[0].id;
+      partner.partner_type_id = partner_type_id;
+      await queryRunner.manager.save(partner);
+
+      await queryRunner.commitTransaction();
+      return true;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      return false;
+    } finally {
+      await queryRunner.release();
     }
-
-    await this.partnerRepository.save({ user_id: user.id, marketing_id: marketing[0].id, partner_type_id: partner_type_id });
-
-    return true;
   }
-  
 
   // create(createUserInput: CreateUserInput) {
   //   return 'This action adds a new user';
